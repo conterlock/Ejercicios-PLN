6249b8bed224d07dbb4752ddb6a44d20fc4dee92
Luis <lmillan131@gmail.com>
2020-05-07 21:38:11 -0400

    Herramientas nltk
    Más Herramientas

--- Aprendiendo/aprendiendo.py
+++ Aprendiendo/aprendiendo.py
@@ -1,3 +1,5 @@
 import nltk
 from nltk.book import *
 text1.concordance("monstrous")  # Ocurrencia de una palabra dada en un texto
+text1.similar("monstrous")      # Palabras asimiladas
+text2.common_contexts(["monstrous", "very"])    # Contextos compartidos entre palabras
